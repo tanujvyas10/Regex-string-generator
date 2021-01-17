bfb91bb1afeb5f0a42285dd5b94febfd2fe284ef
tanuj <tanujvyas10@gmail.com>
2021-01-18 00:19:36 +0530

    minimize the objects values

--- Generator/Tokenizer.js
+++ Generator/Tokenizer.js
@@ -23,22 +23,12 @@ const WHITESPACE = [
   { type: types.CHAR, value: 12 },
   { type: types.CHAR, value: 13 },
   { type: types.CHAR, value: 32 },
-  { type: types.CHAR, value: 160 },
-  { type: types.CHAR, value: 5760 },
-  { type: types.RANGE, from: 8192, to: 8202 },
-  { type: types.CHAR, value: 8232 },
-  { type: types.CHAR, value: 8233 },
-  { type: types.CHAR, value: 8239 },
-  { type: types.CHAR, value: 8287 },
-  { type: types.CHAR, value: 12288 },
-  { type: types.CHAR, value: 65279 },
+  { type: types.CHAR, value: 160 }
 ];
 
 const NOTANYCHAR = [
   { type: types.CHAR, value: 10 },
-  { type: types.CHAR, value: 13 },
-  { type: types.CHAR, value: 8232 },
-  { type: types.CHAR, value: 8233 },
+  { type: types.CHAR, value: 13 }
 ];
 
 const words = { type: types.SET, set: WORDS, not: false };
